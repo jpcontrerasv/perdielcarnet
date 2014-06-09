4d1dfb5e7a109d44d69180fd18644b60371ab855
Juan Pablo Contreras Valdés <juanpablo@wanna.bi>
2014-06-08 23:36:25 -0400

    4

--- wp-content/themes/bootstrapwp/footer.php
+++ wp-content/themes/bootstrapwp/footer.php
@@ -10,6 +10,7 @@
                     <div class="container">
                         <div class="col-lg-12 text-center"><br><br><br><br>
                     <p><small>Hecho con amor por <a href="http://www.wanna.bi">Wannabi</a></small></p>
+                    <p><small>No tenemos nada que ver con el Registro Civil, sólo lo hacemos de buena onda.</small></p>
                 </div>
             </div><!-- /container -->
         </footer>
